af09f769e21ae8697c28d75e174b7d5f56392ac1
ozlemkayyaa <ozlemkayyaa@hotmail.com>
2023-12-30 12:48:58 +0300

    yeni resimler eklendi constantlara eklendi

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,6 +2,8 @@ class TImages {
   // App Logos
   static const String lightAppLogo = "assets/logos/lightLogo.jpg";
   static const String darkAppLogo = "assets/logos/darkLogo.jpg";
+  static const String tLogoMor = "assets/logos/TLogoMor.png";
+  static const String tLogoWhite = "assets/logos/TLogoWhite.png";
 
   // Social Logos
   static const String google = "assets/logos/googleLogo.png";
@@ -9,5 +11,15 @@ class TImages {
 
   // Other Images
   static const String background = "assets/images/background.png";
-  static const String istanbul = "assets/images/istanbul.jpg";
+  static const String istanbulBlack = "assets/images/istanbulBlack.jpg";
+  static const String istanbulWhite = "assets/images/istanbulWhite.jpg";
+  static const String istKod = "assets/images/istkod.png";
+  static const String isImage = "assets/images/is.png";
+
+  // TabBar
+  static const String anket = "assets/images/anket.png";
+  static const String duyuru = "assets/images/duyuru.png";
+  static const String sinav = "assets/images/sınavlar.png";
+  static const String notFound = "assets/images/videoNotFound.jpg";
+  static const String welcome = "assets/images/welcome.png";
 }
