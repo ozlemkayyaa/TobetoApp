d5e94a2a92479d4bee7362bd561673d0d7e63d25
ozlemkayyaa <ozlemkayyaa@hotmail.com>
2024-01-09 02:06:51 +0300

    iletişim1

--- lib/features/education/screens/course/course_screen.dart
+++ lib/features/education/screens/course/course_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:tobeto/features/education/screens/course/widgets/az_button_widget.dart';
